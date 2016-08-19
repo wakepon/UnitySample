52a1104874ff4b4860f603ed3029078c2013a0c2
waken <wakepon@gmail.com>
2016-08-19 16:51:40 +0900

    add FogTest add Imageeffectsample

---
+++ ImageEffectSample/Assets/ToyCameraImageEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ToyCameraImageEffect : CustomImageEffect
+{
+    [SerializeField]
+    [Range(1, 100)]
+    private float m_Size;
+
+    public override string ShaderName
+    {
+        get { return "Hidden/ToyCameraImageEffectShader"; }
+    }
+
+    protected override void UpdateMaterial()
+    {
+        /* Material.SetFloat("_Size", m_Size); */
+    }
+}
